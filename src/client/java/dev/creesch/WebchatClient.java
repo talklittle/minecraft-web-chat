a896d093a3d426cac654c6c78d85d20eabd5ca38
creesch <creesch@users.noreply.github.com>
2024-12-13 14:26:15 +0100

    initial template commit

---
+++ src/client/java/dev/creesch/WebchatClient.java
@@ -0,0 +1,10 @@
+package dev.creesch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WebchatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
